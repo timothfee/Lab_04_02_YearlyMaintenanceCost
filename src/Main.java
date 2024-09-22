fd05ae8002e7ae8dc3af4ff8821d96b328dbd636
Timothy Fee <keys333@proton.me>
2024-09-22 18:52:36 -0400

    Init commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+   public static void main(String[] args) {
+
+    double springExpense = 173.44;
+    double summerExpense = 108.32;
+    double fallExpense = 92.56;
+    double winterExpense = 443.17;
+    double yearlyExpense = springExpense + summerExpense + fallExpense + winterExpense;
+
+    System.out.println("Spring maintenance expense: $" + springExpense);
+    System.out.println("Summer maintenance expense: $" + summerExpense);
+    System.out.println("Fall maintenance expense: $" + fallExpense);
+    System.out.println("Winter maintenance expense: $" + winterExpense);
+    System.out.println("Total yearly maintenance expense: $" + yearlyExpense);
+    
+  }
+}
