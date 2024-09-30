5f709d769288c77721e0c1d8b7d8cb3ca66fcf65
Timothy Fee <keys333@proton.me>
2024-09-29 21:38:17 -0400

    Init commit

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,27 @@
+import java.util.Scanner;
+
 public class Main {
    public static void main(String[] args) {
-
-    double springExpense = 173.44;
-    double summerExpense = 108.32;
-    double fallExpense = 92.56;
-    double winterExpense = 443.17;
-    double yearlyExpense = springExpense + summerExpense + fallExpense + winterExpense;
-
-    System.out.println("Spring maintenance expense: $" + springExpense);
-    System.out.println("Summer maintenance expense: $" + summerExpense);
-    System.out.println("Fall maintenance expense: $" + fallExpense);
-    System.out.println("Winter maintenance expense: $" + winterExpense);
-    System.out.println("Total yearly maintenance expense: $" + yearlyExpense);
+    //Ask for politicalAffiliation affiliation, if input is "D" display democratic donkey, if input is "R" display reblican elephant, if input is "I" display independent person, otherwise display other
     
+    Scanner in = new Scanner(System.in);
+    String politicalAffiliation = "";
+
+    System.out.print("Enter your politicalAffiliation affiliation (D, R, I): ");
+    politicalAffiliation = in.nextLine().toUpperCase();
+
+    if (politicalAffiliation.equals("D")) {
+      System.out.println("You get a Democratic Donkey.");
+    }
+    else if (politicalAffiliation.equals("R")) {
+      System.out.println("You get a Republican Elephant.");
+    }
+    else if (politicalAffiliation.equals("I")) {
+      System.out.println("You get an Independent Person.");
+    } 
+    else {
+      System.out.println("Other.");
+    }
+
   }
 }
